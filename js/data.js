2878be9cf3cccea8823b355c1e77302129bd7c8a
Colin Fuller <cjfuller@gmail.com>
2017-01-08 09:46:43 -0500

    Add more irregular verb cards

--- js/data.js
+++ js/data.js
@@ -1,4 +1,8 @@
-const cardData = [
+//@flow
+
+import {CardType} from "./types.js";
+
+const cardData: CardType[] = [
     {
         front: "commencer; 1st person singular",
         back: "je commence",
@@ -22,6 +26,126 @@ const cardData = [
     {
         front: "commencer; 3rd person plural",
         back: "ils commencent",
+    },
+    {
+        front: "manger; 1st person singular",
+        back: "je mange",
+    },
+    {
+        front: "manger; 2nd person singular",
+        back: "tu manges",
+    },
+    {
+        front: "manger; 3rd person singular",
+        back: "il mange",
+    },
+    {
+        front: "manger; 1st person plural",
+        back: "nous mangeons",
+    },
+    {
+        front: "manger; 2nd person plural",
+        back: "vous mangez",
+    },
+    {
+        front: "manger; 3rd person plural",
+        back: "ils mangent",
+    },
+    {
+        front: "jeter; 1st person singular",
+        back: "je jette",
+    },
+    {
+        front: "jeter; 2nd person singular",
+        back: "tu jettes",
+    },
+    {
+        front: "jeter; 3rd person singular",
+        back: "il jette",
+    },
+    {
+        front: "jeter; 1st person plural",
+        back: "nous jetons",
+    },
+    {
+        front: "jeter; 2nd person plural",
+        back: "vous jetez",
+    },
+    {
+        front: "jeter; 3rd person plural",
+        back: "ils jettent",
+    },
+    {
+        front: "acheter; 1st person singular",
+        back: "j'achète",
+    },
+    {
+        front: "acheter; 2nd person singular",
+        back: "tu achètes",
+    },
+    {
+        front: "acheter; 3rd person singular",
+        back: "il achète",
+    },
+    {
+        front: "acheter; 1st person plural",
+        back: "nous achetons",
+    },
+    {
+        front: "acheter; 2nd person plural",
+        back: "vous achetez",
+    },
+    {
+        front: "acheter; 3rd person plural",
+        back: "ils achètent",
+    },
+    {
+        front: "espérer; 1st person singular",
+        back: "j'espère",
+    },
+    {
+        front: "espérer; 2nd person singular",
+        back: "tu espères",
+    },
+    {
+        front: "espérer; 3rd person singular",
+        back: "il espère",
+    },
+    {
+        front: "espérer; 1st person plural",
+        back: "nous espérons",
+    },
+    {
+        front: "espérer; 2nd person plural",
+        back: "vous espérez",
+    },
+    {
+        front: "espérer; 3rd person plural",
+        back: "ils espèrent",
+    },
+    {
+        front: "essayer; 1st person singular",
+        back: "j'essaie",
+    },
+    {
+        front: "essayer; 2nd person singular",
+        back: "tu essaies",
+    },
+    {
+        front: "essayer; 3rd person singular",
+        back: "il essaie",
+    },
+    {
+        front: "essayer; 1st person plural",
+        back: "nous essayons",
+    },
+    {
+        front: "essayer; 2nd person plural",
+        back: "vous essayez",
+    },
+    {
+        front: "essayer; 3rd person plural",
+        back: "vous essaient",
     },
 ];
 
