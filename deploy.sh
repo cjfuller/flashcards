88590d0f19e63d8b423d968f419fc3f0c1dbed58
Colin Fuller <cjfuller@gmail.com>
2017-01-08 09:14:28 -0500

    Clean up old js versions in the deploy script.

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 git checkout gh_pages
 git merge master
 GIT_HASH=`git log -n 1 --format=%h`
+git rm app_*.js
 npm run-script compile
 mv public/js/app_.js ./app_$GIT_HASH.js
 cat public/index.html | sed -e "s/js\\/app_.js/\\/app_$GIT_HASH.js/g" > ./index.html
