7279e2a427d14d78e33b674662a4b57310018892
Colin Fuller <cjfuller@gmail.com>
2017-01-08 09:27:09 -0500

    Don't load js from an absolute path since site isn't being served from the domain root.

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ GIT_HASH=`git log -n 1 --format=%h`
 git rm app_*.js
 npm run-script compile
 mv public/js/app_.js ./app_$GIT_HASH.js
-cat public/index.html | sed -e "s/js\\/app_.js/\\/app_$GIT_HASH.js/g" > ./index.html
+cat public/index.html | sed -e "s/js\\/app_.js/app_$GIT_HASH.js/g" > ./index.html
 git add index.html
 git add app_$GIT_HASH.js
 git commit -a -m "Deploy to github pages"
